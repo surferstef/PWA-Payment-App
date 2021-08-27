a93bb79a4d95a3ea0982a5c53bdd3ab778f1d3ce
stefan hall <stefhall@comcast.net>
2021-08-27 00:03:26 -0700

    Updating idb.js for online/ offline activity

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -4,35 +4,64 @@ let db;
 const request = indexedDB.open('PWA-Payment-App', 1);
 
 request.onupgradeneeded = function(event) {
-    // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_payment', { autoIncrement: true });
+    db.createObjectStore('new_payment', { autoIncrement: true }); //keyPath: "id" });
   };
 
   // upon a successful 
 request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
+ db = event.target.result;
   
    if (navigator.onLine) {
-     
+     uploadPayment();
     }
   };
   
   request.onerror = function(event) {
-    // log error here
     console.log(event.target.errorCode);
   };
 
-  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_payment'], 'readwrite');
-  
-    // access the object store for `new_pizza`
-    const pizzaObjectStore = transaction.objectStore('new_payment');
-  
-    // add record to your store with add method
-    pizzaObjectStore.add(record);
+    const paymentObjectStore = transaction.objectStore('new_payment');
+    paymentObjectStore.add(record);
   }
+
+  function uploadPayment() {
+    const transaction = db.transaction(['new_payment'], 'readwrite');
+    const paymentObjectStore = transaction.objectStore('new_payment');
+    const getAll = paymentObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/payments', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_payment'], 'readwrite');
+          const paymentObjectStore = transaction.objectStore('new_payment');
+          paymentObjectStore.clear();
+
+          alert('All saved payment has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  }
+
+  // listen for app coming back online
+window.addEventListener('online', uploadPayment);
